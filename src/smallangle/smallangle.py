8d4a090ecb5e51751207c92a89ba990671eb4344
JLuijten <135598395+JLuijten@users.noreply.github.com>
2023-11-16 10:13:52 +0100

    added command groups and click

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,17 +1,28 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def smallangle():
+    pass
+
+
+@smallangle.command()
+@click.argument("number")
 def sin(number):
+    number = int(number)
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
 
+@smallangle.command()
+@click.argument("number")
 def tan(number):
+    number = int(number)
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
